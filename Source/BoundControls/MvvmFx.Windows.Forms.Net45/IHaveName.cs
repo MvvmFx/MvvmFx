b68b2fee8a71fdb0ea62e49117cb726e10a93269
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-07-29 12:04:57 +0100

    Improve code documentation (#89)

--- Source/BoundControls/MvvmFx.Windows.Forms.Net45/IHaveName.cs
+++ Source/BoundControls/MvvmFx.Windows.Forms.Net45/IHaveName.cs
@@ -1,7 +1,16 @@
 ﻿namespace MvvmFx.Windows.Forms
 {
+    /// <summary>
+    /// Defines a component with a Name property.
+    /// </summary>
     public interface IHaveName
     {
+        /// <summary>
+        /// Gets or sets the Name property of the component object.
+        /// </summary>
+        /// <value>
+        /// The component Name.
+        /// </value>
         string Name { get; set; }
     }
 }
