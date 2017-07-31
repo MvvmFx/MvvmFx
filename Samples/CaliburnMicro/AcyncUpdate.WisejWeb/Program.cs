c40fce12087ed99a88bbbc9fa6b3c3d9a9634016
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-07-31 17:52:38 +0100

    Add CaliburnMicro samples (empty for now).

---
+++ Samples/CaliburnMicro/AcyncUpdate.WisejWeb/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Wisej.Web;
+
+namespace AcyncUpdate.WisejWeb
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            Window1 window = new Window1();
+            window.Show();
+        }
+
+        //
+        // You can use the entry method below
+        // to receive the parameters from the URL in the args collection.
+        //
+        //static void Main(NameValueCollection args)
+        //{
+        //}
+    }
+}
---
+++ Samples/CaliburnMicro/AcyncUpdate.WisejWeb/Window1.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Wisej.Web;
+
+namespace AcyncUpdate.WisejWeb
+{
+    public partial class Window1 : Form
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+}
