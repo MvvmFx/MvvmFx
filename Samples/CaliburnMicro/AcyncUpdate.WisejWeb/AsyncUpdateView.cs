d9e66fa056b7b47a2c5f25869f84d0e7fb81abbf
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-08-06 16:35:19 +0100

    Port sample AsyncUpdate to CaliburnMicro Wisej (#110)

---
+++ Samples/CaliburnMicro/AcyncUpdate.WisejWeb/AsyncUpdateView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+#if WISEJ
+using Wisej.Web;
+#else
+using System.Windows.Forms;
+#endif
+using MvvmFx.CaliburnMicro;
+
+namespace AcyncUpdate.UI
+{
+    public interface IAsyncUpdateView : IHaveDataContext, IHaveBusyIndicator
+    {
+    }
+
+    public partial class AsyncUpdateView : Form, IAsyncUpdateView
+    {
+        private IAsyncUpdateViewModel _viewModel;
+
+        public AsyncUpdateView()
+        {
+            InitializeComponent();
+            ActiveControl = customerName;
+        }
+
+        public BusyIndicator Indicator
+        {
+            get { return busyIndicator; }
+        }
+
+        private void Bind()
+        {
+            customerName.DataBindings.Add(new Binding("ReadOnly", DataContext, "IsCustomerNameReadOnly", true,
+                DataSourceUpdateMode.OnPropertyChanged));
+        }
+
+        #region IHaveDataContext implementation
+
+        public event EventHandler<DataContextChangedEventArgs> DataContextChanged = delegate { };
+
+        public object DataContext
+        {
+            get { return _viewModel; }
+            set
+            {
+                if (value != _viewModel)
+                {
+                    _viewModel = value as IAsyncUpdateViewModel;
+                    Bind();
+                    DataContextChanged(this, new DataContextChangedEventArgs());
+                }
+            }
+        }
+
+        #endregion
+    }
+}
