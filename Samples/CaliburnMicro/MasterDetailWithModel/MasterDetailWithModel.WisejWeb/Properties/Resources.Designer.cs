ef1ccb03c6fe20f033ec9d4416478e40d88bc574
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-08-06 17:01:44 +0100

    Port samples to CaliburnMicro Wisej (#110)

--- Samples/CaliburnMicro/MasterDetailWithModel/MasterDetailWithModel.WisejWeb/Properties/Resources.Designer.cs
+++ Samples/CaliburnMicro/MasterDetailWithModel/MasterDetailWithModel.WisejWeb/Properties/Resources.Designer.cs
@@ -59,5 +59,45 @@ namespace MasterDetailWithModel.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap AddNew16 {
+            get {
+                object obj = ResourceManager.GetObject("AddNew16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Close16 {
+            get {
+                object obj = ResourceManager.GetObject("Close16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Delete16 {
+            get {
+                object obj = ResourceManager.GetObject("Delete16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Save16 {
+            get {
+                object obj = ResourceManager.GetObject("Save16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
