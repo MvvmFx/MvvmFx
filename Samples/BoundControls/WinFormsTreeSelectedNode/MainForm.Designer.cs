bb65d94369cb14bf9031bf275f18948827fbe9b4
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-08-21 18:27:42 +0100

    Sample for Test Wisej TreeView SelectedNode issue (#126)

---
+++ Samples/BoundControls/WinFormsTreeSelectedNode/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WinFormsTreeSelectedNode
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Wisej Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeNode11 = new System.Windows.Forms.TreeNode("Node 1.1");
+            this.treeNode1 = new System.Windows.Forms.TreeNode("Node 1", new System.Windows.Forms.TreeNode[] {
+            treeNode11});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeNode1
+            // 
+            this.treeNode1.Name = "Node1";
+            this.treeNode1.Text = "Node 1";
+            // 
+            // treeNode11
+            // 
+            this.treeNode11.Name = "Node11";
+            this.treeNode11.Text = "Node 1.1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(284, 400);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeSelect);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(0, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Step 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 461);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeNode treeNode1;
+        private System.Windows.Forms.TreeNode treeNode11;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Samples/BoundControls/WinFormsTreeSelectedNode/MainForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsTreeSelectedNode
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            treeView1.ExpandAll();
+        }
+
+        private void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            MessageBox.Show(string.Format("BeforeSelect - node: {0}", e.Node == null ? "NULL" : e.Node.Text),
+                "Trace", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            MessageBox.Show(string.Format("AfterSelect - node: {0}", e.Node == null ? "NULL" : e.Node.Text),
+                "Trace", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode == treeNode11)
+            {
+                MessageBox.Show("Select null node", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                treeView1.SelectedNode = null;
+                MessageBox.Show("End Step 2\r\nNote no Select event will be raised.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button1.Text = "Step 1";
+            }
+            else
+            {
+                MessageBox.Show("Select Node 1.1", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                treeView1.SelectedNode = treeNode11;
+                treeView1.Focus();
+                MessageBox.Show("End Step 1", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button1.Text = "Step 2";
+            }
+        }
+    }
+}
