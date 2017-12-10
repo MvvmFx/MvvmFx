1e57d4f0ccd6d5817286cef05a9a5f0399e0ef65
tfreitasleal <tfreitasleal@gmail.com>
2017-12-10 00:07:04 +0000

    Enhance sample.

--- Samples/BoundControls/WinFormsMenuBinding/MainForm.Designer.cs
+++ Samples/BoundControls/WinFormsMenuBinding/MainForm.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(250, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "WinForms Bind Menu";
+            this.button1.Text = "WinForms Bindings";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -50,7 +50,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(250, 40);
             this.button2.TabIndex = 1;
-            this.button2.Text = "MvvmFx Bind Menu";
+            this.button2.Text = "MvvmFx Bindings";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
--- Samples/BoundControls/WisejWebMenuBinding/MainPage.Designer.cs
+++ Samples/BoundControls/WisejWebMenuBinding/MainPage.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.button1 = new Wisej.Web.Button();
             this.button2 = new Wisej.Web.Button();
-            this.button3 = new Wisej.Web.Button();
-            this.button4 = new Wisej.Web.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(250, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "WinForms Bind Menu";
+            this.button1.Text = "WinForms Bindings";
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
@@ -51,35 +49,13 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(250, 40);
             this.button2.TabIndex = 1;
-            this.button2.Text = "MvvmFx Bind Menu";
+            this.button2.Text = "MvvmFx Bindings";
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("default", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.button3.Location = new System.Drawing.Point(50, 250);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(250, 40);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "WinForms Bind ToolBar";
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("default", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.button4.Location = new System.Drawing.Point(50, 350);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(250, 40);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "MvvmFx Bind ToolBar";
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "MainPage";
@@ -93,7 +69,5 @@
 
         private Wisej.Web.Button button1;
         private Wisej.Web.Button button2;
-        private Wisej.Web.Button button3;
-        private Wisej.Web.Button button4;
     }
 }
