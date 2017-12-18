785f3bdd87fe0aaa08fae8a1690cd74215d03bde
tfreitasleal <tfreitasleal@gmail.com>
2017-12-18 19:45:44 +0000

    Wisej session doesn't timeout any more.

--- Samples/CaliburnMicro/CslaSample/CslaSample.WinForms/Program.cs
+++ Samples/CaliburnMicro/CslaSample/CslaSample.WinForms/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 #if WINFORMS
 using System.Windows.Forms;
+#else
+using Wisej.Web;
 #endif
 
 namespace CslaSample
@@ -16,9 +18,19 @@ namespace CslaSample
 #if WINFORMS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+#else
+            Application.SessionTimeout += Application_SessionTimeout;
 #endif
 
             new AppBootstrapper().Run();
         }
+
+#if WISEJ
+        private static void Application_SessionTimeout(object sender, System.ComponentModel.HandledEventArgs e)
+        {
+            // you can display a form and override the default session timeout dialog.
+            e.Handled = true;
+        }
+#endif
     }
 }
