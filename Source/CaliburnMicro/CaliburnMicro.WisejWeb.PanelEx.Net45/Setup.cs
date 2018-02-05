b6b686ab38cc5c37db9d00f844947a31449aea8a
tfreitasleal <tfreitasleal@gmail.com>
2018-02-05 22:00:13 +0000

    PanelEx with ProxyAgent (#157) (phase 1 completed)

---
+++ Source/CaliburnMicro/CaliburnMicro.WisejWeb.PanelEx.Net45/Setup.cs
@@ -0,0 +1,41 @@
+﻿using MvvmFx.CaliburnMicro;
+using MvvmFx.CaliburnMicro.ComponentProxy;
+
+namespace CaliburnMicro.WisejWeb.PanelEx
+{
+    /// <summary>
+    /// Setup this Wisej extension on CaliburnMicro.WisejWeb.
+    /// </summary>
+    public static class Setup
+    {
+        private static bool _setupDone;
+
+        /// <summary>
+        /// Runs ProxyAgent and ElementConvention configurations.
+        /// </summary>
+        public static void Run()
+        {
+            if (_setupDone)
+                return;
+
+            ConfigureProxyAgent();
+            ConfigureElementConvention();
+
+            _setupDone = true;
+        }
+
+        private static void ConfigureProxyAgent()
+        {
+            ProxyManager.AddProxyAgent<PanelEx>(PanelExAgent.GetNamedItems, PanelExAgent.BindVisualProperties);
+        }
+
+        private static void ConfigureElementConvention()
+        {
+            ConventionManager.AddElementConvention<ComponentToolExProxy>("Name", null, "Click")
+                .CreateAction = (element, methodName, parameters) =>
+            {
+                return new ActionMessage(element, "Click", methodName, parameters);
+            };
+        }
+    }
+}
