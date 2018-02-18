42f62160bd10cc23dffebdc0a6d7f5773ece49f2
tfreitasleal <tfreitasleal@gmail.com>
2018-02-18 11:48:30 +0000

    Rename namespace and assemblies for bindings (#184) - Rename namespaces

--- Source/Binding/MvvmFx.Windows.WinForms.Net45/Properties/AssemblyInfo.cs
+++ Source/Binding/MvvmFx.Windows.WinForms.Net45/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Reflection;
 //using System.Security;
 
-[assembly: AssemblyTitle("MvvmFx.Windows for Windows Forms in NET 4.5")]
-[assembly: AssemblyProduct("MvvmFx.Windows.WinForms.NET45")]
+[assembly: AssemblyTitle("MvvmFx.Bindings for Windows Forms in NET 4.5")]
+[assembly: AssemblyProduct("MvvmFx.Bindings.WinForms.NET45")]
 [assembly: AssemblyDescription("A POCO (Plain Old CLR Object) and command binding library.")]
 [assembly: AssemblyCopyright("Copyright © Kent Boogaart (parts), Tiago Freitas Leal.")]
 [assembly: AssemblyCompany("Kent Boogaart and MvvmFx Project")]
